4fd895744bebb8604215c508a2e97b1326845e31
Islam Ahmed <IMahmed@conestogac.on.ca>
2025-03-01 17:17:29 -0500

    TFP-16 made buttons go use use navigate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,49 +3,50 @@ import "./App.css";
 import { BsStars } from "react-icons/bs";
 import { FaHeart, FaShoppingBag } from "react-icons/fa";
 import { GoGoal } from "react-icons/go";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import MenuNav from "./components/menu-nav/MenuNav";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 
-const menuItems = [
-  {
-    icon: <GoGoal />,
-    id: 1,
-    label: "Goals",
-    onClick: () => {
-      console.log("Goals clicked");
-    }
-  },
-  {
-    badge: 3,
-    icon: <FaHeart />,
-    id: 2,
-    label: "Buddy",
-    onClick: () => {
-      console.log("Buddy clicked");
-    }
-  },
-  {
-    icon: <FaShoppingBag />,
-    id: 3,
-    label: "Shop",
-    onClick: () => {
-      console.log("Shop clicked");
-    }
-  },
-  {
-    icon: <BsStars />,
-    id: 4,
-    label: "Perks",
-    onClick: () => {
-      console.log("Perks clicked");
-    }
-  }
-];
-
 function App() {
+  const navigate = useNavigate();
+
+  const menuItems = [
+    {
+      icon: <GoGoal />,
+      id: 1,
+      label: "Goals",
+      onClick: () => {
+        void navigate("/");
+      }
+    },
+    {
+      badge: 3,
+      icon: <FaHeart />,
+      id: 2,
+      label: "Buddy",
+      onClick: () => {
+        void navigate("/buddy");
+      }
+    },
+    {
+      icon: <FaShoppingBag />,
+      id: 3,
+      label: "Shop",
+      onClick: () => {
+        void navigate("/shop");
+      }
+    },
+    {
+      icon: <BsStars />,
+      id: 4,
+      label: "Perks",
+      onClick: () => {
+        void navigate("/perks");
+      }
+    }
+  ];
   return (
     <main>
       <Routes>
