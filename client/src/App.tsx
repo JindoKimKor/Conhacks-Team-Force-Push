dbb6fbe31a3ee5143ac962c5c9c2607c9a14e3d2
NipunGrover <ngrover1@conestogac.on.ca>
2025-03-01 15:17:13 -0500

    TFP-6 added homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
+import Homepage from "./pages/Homepage/Homepage";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <main>
       <Routes>
+        <Route element={<Homepage />} path="/" />
         <Route element={<Login />} path="/login" />
       </Routes>
     </main>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// Get the directory name correctly in ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
