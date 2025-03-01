4ee0aca7a6b13ff9ef8dd9a0d37a0c15ed06db37
SalmanNouman <114771256+SalmanNouman@users.noreply.github.com>
2025-03-01 12:05:30 -0500

    TFP-1 Setup mongoose for the server.

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import express, { json, urlencoded } from "express";
 
+import connectDB from "./src/config/database.js";
 import router from "./src/routes/index.js";
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 // ------ Create the Express app -----
 const app = express();
@@ -21,7 +29,7 @@ app.use(urlencoded({ extended: true }));
 app.use(`/api`, router);
 
 // ------ Start the server -----
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
